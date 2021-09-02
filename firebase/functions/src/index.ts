496422ea2be4f2c4c2c7dc2b4f3ef67e5e8b3682
HosokawaR <thin.river.jp@gmail.com>
2021-09-03 00:02:00 +0900

    functions の lint error を修正

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -17,8 +17,8 @@ type AuthorityClaims = {
 };
 
 const grantAuthorityByEmail = async (
-  email: string,
-  customClaims: AuthorityClaims
+    email: string,
+    customClaims: AuthorityClaims
 ) => {
   const user = await auth.getUserByEmail(email);
   await auth.setCustomUserClaims(user.uid, customClaims);
@@ -28,18 +28,18 @@ export const reflectAuthority = functions.https.onCall(async () => {
   try {
     const doc = await db.doc("system/authority").get();
     if (!doc.exists) {
-      return { status: 500 , message: "権限設定ドキュメントを Firestore に設置したください。"};
+      return {status: 500, message: "権限設定ドキュメントを Firestore に設置したください。"};
     }
-    const { admins, auditors } = doc.data() as FirebaseAuthority;
+    const {admins, auditors} = doc.data() as FirebaseAuthority;
     const adminPromises = admins.map((email) =>
-      grantAuthorityByEmail(email, { admin: true, auditor: true })
+      grantAuthorityByEmail(email, {admin: true, auditor: true})
     );
     const auditorPromises = auditors.map((email) =>
-      grantAuthorityByEmail(email, { admin: false, auditor: true })
+      grantAuthorityByEmail(email, {admin: false, auditor: true})
     );
     await Promise.all([...adminPromises, ...auditorPromises]);
-    return { status: 200, message: "全ての権限が更新されました。" };
+    return {status: 200, message: "全ての権限が更新されました。"};
   } catch (e) {
-    return { status: 500, message: String(e) };
+    return {status: 500, message: String(e)};
   }
 });
